776507e0bf2656ad7648b56a06fe06cd2a1aded2
Michal Vala <mvala@redhat.com>
2021-03-19 16:19:51 +0100

    kube-rbac-proxy protected app

--- kube-rbac-proxy/01_deploy.sh
+++ kube-rbac-proxy/01_deploy.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# deploy dex
-sed "s/{{MINIKUBE_IP}}/$( minikube ip )/g" deploy.yaml | kubectl apply -n che -f -
+NAMESPACE=${1}
+USER=${2:-${NAMESPACE}@che}
 
-until [ $( kc get ingress -n che | grep kube-rbac-app | grep -o $( minikube ip ) | wc -l) -eq 2 ]; do echo "Waiting for ingress ..."; sleep 3; done
+if [ -z ${NAMESPACE} ]; then
+  echo "you have to define the namespace './01_deploy <namespace>'"
+  exit 1
+fi
+
+# deploy app with kube-rbac-proxy
+sed "s/{{MINIKUBE_IP}}/$( minikube ip )/g; s/{{NAMESPACE}}/${NAMESPACE}/g" deploy.yaml | kubectl apply -n ${NAMESPACE} -f -
+
+until [ $( kubectl get ingress -n ${NAMESPACE} | grep kube-rbac-app | grep -o $( minikube ip ) | wc -l) -eq 2 ]; do echo "Waiting for ingress ..."; sleep 3; done
 echo
-echo "http://kube-rbac-app.$( minikube ip ).nip.io"
+echo "http://${NAMESPACE}-kube-rbac-app.$( minikube ip ).nip.io"
 echo
