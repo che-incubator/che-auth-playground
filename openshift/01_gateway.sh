62d9cafcc7fe5e7fe5a4ee64c9329a16f0f6c08b
Michal Vala <mvala@redhat.com>
2021-04-01 14:13:22 +0200

    openshift up

---
+++ openshift/01_gateway.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+NAMESPACE=che
+HOSTNAME=apps.cluster-5eb2.5eb2.sandbox810.opentlc.com
+
+kubectl create namespace ${NAMESPACE}
+
+# kubectl create secret tls che-tls --key=../minikube_dex/ssl/key.pem --cert=../minikube_dex/ssl/cert.pem -n ${NAMESPACE} || true
+
+cat rbac.yaml | kubectl apply -n ${NAMESPACE} -f -
+sed "s/{{HOSTNAME}}/${HOSTNAME}/g" oauth.yaml | kubectl apply -n ${NAMESPACE} -f -
+sed "s/{{HOSTNAME}}/${HOSTNAME}/g" o2p_config.yaml | kubectl apply -n ${NAMESPACE} -f -
+cat traefik_config.yaml | kubectl apply -n ${NAMESPACE} -f -
+sed "s/{{HOSTNAME}}/${HOSTNAME}/g" deploy.yaml | kubectl apply -n ${NAMESPACE} -f -
+cat testapp.yaml | kubectl apply -n ${NAMESPACE} -f -
+
+# until [ $( kubectl get ingress -n ${NAMESPACE} | grep gateway | grep -o $( minikube ip ) | wc -l) -eq 2 ]; do echo "Waiting for ingress ..."; sleep 3; done
+# echo
+# echo "http://che.$( minikube ip ).nip.io"
+# echo
